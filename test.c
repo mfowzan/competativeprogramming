0a1e6621e2cf55f6c7492484fc3fe05cd0520a5f
Mohammad Fowzan <mohdfowzanshalla@gmail.com>
2024-12-27 23:20:24 +0530

    created my first header file

--- test.c
+++ test.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include "fowzan.h"
 
 int main (void){
 
-    printf("hello");
+    printf("hello\n");
+    printf("%f",PI);
+    int arr[]={1,2,3,4};
+    int size= sizeof(arr);
+    printf("lakhan ath%f",PI);
+    printf("%d", len(arr,size));
     return 0;
 
 

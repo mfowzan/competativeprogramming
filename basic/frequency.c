16f739379d864d05d1ecba17f1fc1b07ba7083c0
Mohammad Fowzan <mohdfowzanshalla@gmail.com>
2025-01-02 22:13:33 +0530

    improved the code

--- basic/frequency.c
+++ basic/frequency.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
 
 void frequency(char str[]);
 int main(int argc, char* argv[])
@@ -12,32 +13,27 @@ int main(int argc, char* argv[])
 
   frequency(argv[1]);
 }
-
+/*we create an array count[] which can store 26 integers, we use it to store the count of each character, basically converting each character into their ascii value and finding their
+ascii value, for eg for small a, ascii value is 97, so we subtract it from 97, 97-97=0, so its index in count[] will be 0. similarly we do it for all characters in a string */
 void frequency(char str[])
 {
-    int asci=0;
     int count[26]={0};
     int l=strlen(str);
     for (int i=0;i<l;i++)
+    {   
+        if (isalnum(str[i]))
     {
-        asci=str[i];
-        printf("%d\n",asci);
-        printf("%c\n",asci);
-        count[asci-97]++;
+        char chr=tolower(str[i]);
+        count[chr-'a']++;
+    }
     }
 
-    for (int k=0;k<26;k++)
-    {
-        printf("%c : %d\n",k+97,count[k]);
-        
-    }
-    printf("\n\n");
 
-    for (int j=0;j<26;j++)
+    for (int i=0;i<26;i++)
     {
-        if (count[j]>0)
+        if (count[i]>0)
         {
-            printf("%c : %d\n",j+97,count[j]);
+            printf("%c : %d\n",i+97,count[i]);
         }
     }
 }

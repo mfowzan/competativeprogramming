8a135cc030a89ee0a264af50e2d18819e6b7c19c
Mohammad Fowzan <mohdfowzanshalla@gmail.com>
2025-01-07 20:54:25 +0530

    solved two sum problem using brute force

---
+++ leetcode/twosum.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target ) 
+
+{
+    int* ans = (int*)malloc(2 * sizeof(int));
+    for (int i=0;i<numsSize-1;i++)
+    {
+        for (int j=i+1;j<numsSize;j++)
+        {
+            if (nums[i]+nums[j]==target)
+            {
+            ans[0]=i;
+            ans[1]=j;
+            return ans;
+            }
+        }
+    }
+}
+
+int main(void)
+{
+    int arr[]={2,7,3,11,5,15};
+    int target=20;
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int* ptr= twoSum(arr,size,target);
+    printf("%d  %d ",ptr[0],ptr[1]);
+
+    return 0;
+
+}
